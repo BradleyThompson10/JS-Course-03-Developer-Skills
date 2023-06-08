fdfe7ab74e126d03e336a34f8d1e8723e28caad2
BradleyThompson10 <brdthompson4@gmail.com>
2023-06-08 09:31:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,20 @@
 // The problem
 // Calculate the temperature amplitude from an array of temps, keeping in mind there will be errors
 
+//second problem
+//cacluate using mutiple arrays
+
 const temperatures = [3, -2, -6, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [4, 5, 6, 19, 15, 14, 9, 3, 7, 'error', 12];
+const MergedTemperatures = temperatures.concat(temperatures2);
+
+//first way to solve the problem 
 
 function numberfilter(element) {
   return typeof element === 'number';
 }
 
-const filterTemperatures = temperatures.filter(numberfilter);
+const filterTemperatures = MergedTemperatures.filter(numberfilter);
 console.table(filterTemperatures);
 
 let max = Math.max(...filterTemperatures);
@@ -24,3 +31,25 @@ const calcTempAmplitude = function (maxTemp, minTemp) {
 
 let tempAmplitude = calcTempAmplitude(max, min);
 console.log(tempAmplitude);
+
+//Another way to solve the problem 
+
+// const calcTempAmplitude = function (t1, t2) {
+//   const temps = t1.concat(t2);
+
+//  let max = temps[0];
+//  let min = temps[0];
+
+//  for(let i = 0; i < temps.length; i++) {
+//   const curTemp = temps[i]
+//   if(typeof curTemp !== 'number') continue
+
+//   if(curTemp > max) max = curTemp;
+//   if(curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min
+// };
+
+// const tempAmplitude = calcTempAmplitude(temperatures, temperatures2)
+// console.log(tempAmplitude);
